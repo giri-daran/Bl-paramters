cb4d689274da41c2cd745b4f099526fcdabaf3f7
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-01 11:14:36 +0530

    Update main.js

--- BP/Assets/js/main.js
+++ BP/Assets/js/main.js
@@ -1,2 +1,5 @@
-$(function(){$(".lazy").lazy({effect:"fadeIn",effectTime:500,threshold:0})}),
-$(document).ready(function(){$("#Parameters").scrollTop(320)});
+$(function()
+  {$(".lazy").lazy({effect:"fadeIn",effectTime:500,threshold:0})}),
+$(document).ready(function(){
+  $("#Parameters").scrollTop(320)
+});
